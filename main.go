9c27675cc6f8a87b2946959f0ea0ad03f895dfd7
chiccs <42506969+chiccs@users.noreply.github.com>
2022-08-08 19:45:00 +0800

    Update main.go

--- main.go
+++ main.go
@@ -98,14 +98,14 @@ import (
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/novel"          // 铅笔小说网搜索
 	//_ "github.com/FloatTech/ZeroBot-Plugin/plugin/nsfw"           // nsfw图片识别
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/omikuji" // 浅草寺求签
-	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/qqwife"         // 一群一天一夫一妻制群老婆
+	 _ "github.com/FloatTech/ZeroBot-Plugin/plugin/qqwife"         // 一群一天一夫一妻制群老婆
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/reborn"         // 投胎
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/runcode" // 在线运行代码
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/saucenao" // 以图搜图
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/scale" // 叔叔的AI二次元图片放大
 
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/score"          // 分数
-	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/setutime" // 来份涩图
+	 _ "github.com/FloatTech/ZeroBot-Plugin/plugin/setutime" // 来份涩图
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/shadiao"        // 沙雕app
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/shindan"        // 测定
 	//_ "github.com/FloatTech/ZeroBot-Plugin/plugin/tarot"          // 抽塔罗牌
